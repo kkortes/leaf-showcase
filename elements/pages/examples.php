75646b09bef3987c7b97fa9f53d394cc3a5f1e73
kkortes <kim@kortes.se>
2014-07-17 18:01:15 +0200

    added examples page

---
+++ elements/pages/examples.php
@@ -0,0 +1,36 @@
+<div>
+	<div class="crow">
+		<div class="ws-12">
+			<div class="container">
+				<h1>Examples</h1>
+				<p>Here's some examples on what you can do with Leaf.</p>
+				<p>Resize your browser window.
+				</p>
+				<div class="spacer"></div>
+
+				<div class="image platform">
+					<img src="<?=$app->url?>assets/image/imac.png" style="width:100%; opacity:0;" class="desktop-nav" />
+					<img src="<?=$app->url?>assets/image/iphone.png" style="width:100%; opacity:0;" class="show-on-mobile" />
+					<img src="<?=$app->url?>assets/image/ipad.png" style="width:100%; opacity:0;" class="show-on-tablet" />
+					<div class="inside">
+						<?=$app->get('parts/mini-web')?>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="ws-12">
+
+		<!-- 	<div class="ws-12 crow fly">
+				<div style="text-align:center;">
+					
+				</div>
+				<div style="text-align:center;">
+					<img src="<?=$app->url?>assets/image/ipad.png" style="max-width:400px;" />
+				</div>
+				<div style="text-align:center;">
+					<img src="<?=$app->url?>assets/image/iphone.png" style="max-width:800px;" />
+				</div>
+			</div> -->
+		</div>
+	</div>
+</div>
---
+++ elements/parts/mini-web.php
@@ -0,0 +1,47 @@
+<div class="clearfix text-white" style="background:#fafafa;">
+	<div class="fill-blue clearfix">
+		<div class="pull-right" style="padding:5px 10px;">
+			<i class="icon icon-signal"></i>&nbsp;&nbsp;<i class="icon icon-screen desktop-nav"></i><i class="icon icon-bolt show-on-tablet"></i><i class="icon icon-mobile show-on-mobile"></i>&nbsp;&nbsp;<?=date('H:i')?>
+		</div>
+	</div>
+	<div class="fill-blue fill-minus-2 base-padding clearfix">
+		<div class="relative pull-left" style="padding-top:4px;">
+			<i class="icon icon-list2 text-white" style="font-size:24px;line-height:24px;"></i>
+			<div class="drop down left fill-white card" style="width:200px;">
+				<ul class="unstyled">
+					<li>
+						<a href="#" class="list-item">LINK ONE</a>
+					</li>
+					<li>
+						<a href="#" class="list-item">LINK TWO</a>
+					</li>
+					<li>
+						<a href="#" class="list-item">LINK THREE</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="text" style="padding-left:25px;">
+			<h2 style="margin:0 0 2px 0;">Nature Images</h2>
+		</div>
+	</div>
+
+	<?
+	$examples = array('nature', 'forest', 'woods', 'backyard', 'heaven', 'sea', 'cliff');
+	?>
+
+	<div class="base-padding" style="padding-right:0;">
+		<? foreach($examples as $ex) { ?>
+		<div class="tile fill-white pull-left text-grey text-plus-3" style="margin:0 8px 8px 0;">
+			<div class="image" style="max-height:100px;overflow:hidden;">
+				<img src="http://<?=$ex?>.jpg.to" />
+			</div>
+			<div class="text base-padding">
+				Image of <?=ucfirst($ex)?><br /><br />
+				<a href="#" class="button text text-grey text-plus-3">Share</a>
+				<a href="#" class="button text text-orange">Open map</a>
+			</div>
+		</div>
+		<? } ?>
+	</div>
+</div>
