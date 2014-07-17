a89fffa9b0faea96a5d71563ee85295d2ce22495
kkortes <kim@kortes.se>
2014-07-17 16:30:36 +0200

    icomoon and waves are now added with base.styl instead of in header

--- elements/header.php
+++ elements/header.php
@@ -7,9 +7,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <link rel="icon" type="image/png" href="<?=$app->url?>favicon.ico?v=4" />
 
-  <link rel="stylesheet" type="text/css" href="<?=$app->url?>leaf/fonts/icomoon/style.css" />
   <link rel="stylesheet" type="text/css" href="<?=$app->url?>leaf/css/base.css" />
-  <link rel="stylesheet" type="text/css" href="<?=$app->url?>leaf/js/libs/waves/waves.css" />
   <link rel="stylesheet" type="text/css" href="<?=$app->url?>assets/css/style.css" />
 
   <link href='http://fonts.googleapis.com/css?family=Roboto:400,900italic,900,700italic,700,500,500italic,400italic,300italic,300,100italic,100' rel='stylesheet' type='text/css' />
