7ee92b25d7d987e4c4be3400b58830ec30b76bb7
kkortes <kim@kortes.se>
2014-07-17 16:23:26 +0200

    added new startpage image

--- elements/pages/index.php
+++ elements/pages/index.php
@@ -1,12 +1,12 @@
 <div class="fullwidth">
   <div class="crow" style="padding-bottom:100px;">
-    <div class="ws-12 text-center">
+    <div class="ws-12 text-center text-white">
       <h1>
       <i class="icon icon-leaf text-green"></i>
-      <br />A CSS framework based on <a href="http://google.com/design/spec/" target="_blank" class="text-blue-grey plus-2">Google's material design</a>
+      <br />A CSS framework based on <a href="http://google.com/design/spec/" target="_blank">Google's material design</a>
       </h1>
       <h2 class="inline-block" style="width:60%;">
-      <i class="icon icon-warning"></i> Leaf is in beta and could be used for production. <br /> Though be aware, there's still much todo and some functionality may not be fully compatible. <br />I appreciate all the feedback I get.<br />
+      <i class="icon icon-warning text-yellow"></i> Leaf is in beta and could be used for production. <br /> Though be aware, there's still much todo and some functionality may not be fully compatible. <br />I appreciate all the feedback I get.<br />
       <a href="/getting-started" class="small">Read more</a>
       </h2>
     </div>
