ef4ee7da53e8b453626ebcbd120c6e8f5b534528
kkortes <kim@kortes.se>
2014-07-17 18:11:13 +0200

    new addition

--- elements/pages/news.php
+++ elements/pages/news.php
@@ -6,6 +6,13 @@
 			<h2>News</h2>
 			<hr />
 
+			<h3>Bower power! <br /> <span class="small text-grey">17 July 2014</span></h3>
+			<p>
+			Leaf is now added to <a href="http://bower.io/" target="_blank">Bower</a>'s package list, for those who want to download it at instant speed.
+			</p>
+
+			<hr />
+
 			<h3>Leaf CSS Framework has now entered beta! <br /> <span class="small text-grey">17 July 2014</span></h3>
 			<p>
 			The development of Leaf has been rapid during the few passed days. More additions are coming up every day and I feel like the framework now usable.
@@ -39,9 +46,17 @@
 			<strong class="label">17 July</strong>
 			<ul class="unstyled">
 				<li>
+					<div class="list-item">
+						<div class="text">
+							Waves and icomoon is now a part of Leaf.<br />
+							<span class="text-grey small">This means you do not have to include them by youself inside your <?=htmlspecialchars('<head>')?>-tag.</span>
+						</div>
+					</div>
+				</li>
+				<li>
 					<a href="<?=$app->url?>component/type/images" class="list-item">
 						<div class="text">
-							Image added as its own component.<br />
+							<i class="icon icon-forward"></i> Image added as its own component.<br />
 							<span class="text-grey small">Checkout it out here.</span>
 						</div>
 					</a>
@@ -49,7 +64,7 @@
 				<li>
 					<a href="<?=$app->url?>component/type/tabs" class="list-item">
 						<div class="text">
-							Tabs added as its own component.<br />
+							<i class="icon icon-forward"></i> Tabs added as its own component.<br />
 							<span class="text-grey small">Checkout it out here.</span>
 						</div>
 					</a>
@@ -57,7 +72,7 @@
 				<li>
 					<a href="<?=$app->url?>icons" class="list-item">
 						<div class="text">
-							Icons & icon page added.<br />
+							<i class="icon icon-forward"></i> Icons & icon page added.<br />
 							<span class="text-grey small">Checkout Leaf's sweet icons provided by Icomoon.</span>
 						</div>
 					</a>
@@ -65,7 +80,7 @@
 				<li>
 					<a href="<?=$app->url?>component/type/slider" class="list-item">
 						<div class="text">
-							Touch gestures framework added.<br />
+							<i class="icon icon-forward"></i> Touch gestures framework added.<br />
 							<span class="text-grey small">Hammer.js added for touch gestures. A demo of this can be viewed on slider component page. Swipe it left or right.</span>
 						</div>
 					</a>
@@ -79,7 +94,7 @@
 				<li>
 					<a href="<?=$app->url?>component/type/buttons" class="list-item">
 						<div class="text">
-							Waves added.<br />
+							<i class="icon icon-forward"></i> Waves added.<br />
 							<span class="text-grey small">Waves is a click effect event, read more and try it out here.</span>
 						</div>
 					</a>
