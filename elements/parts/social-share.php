2ea8ce2232fc9c1bfc00911bf2fca73a0ddc9526
kkortes <kim@kortes.se>
2015-01-15 16:31:43 +0100

    cleanup

--- elements/parts/social-share.php
+++ elements/parts/social-share.php
@@ -23,14 +23,13 @@
             </form>
           </div>
 
-            <div class="drop down-left" style="padding-left:10px;padding-top:5px;">
-              <div class="relative">
-                <div class="image card bg-white" style="padding:3px;">
-                  <img src="http://s11.postimg.org/8yxr24w0v/bitcoin.png" />
-                </div>
-                <div class="drop bottom-left bg-white card" style="width:310px;padding:5px;">
-                  1PiRdutMwncxkjp8quxQiF1qFUBGBjwhfg
-                </div>
+          <div class="drop down-left" style="padding-left:10px;padding-top:5px;">
+            <div class="relative">
+              <div class="image card bg-white" style="padding:3px;">
+                <img src="http://s11.postimg.org/8yxr24w0v/bitcoin.png" />
+              </div>
+              <div class="drop bottom-left bg-white card" style="width:310px;padding:5px;">
+                1PiRdutMwncxkjp8quxQiF1qFUBGBjwhfg
               </div>
             </div>
           </div>
